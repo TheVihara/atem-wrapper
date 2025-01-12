a7a156c0c33d48af069dffdd3846c5e409869443
Vihara <gorenjecalen@gmail.com>
2025-01-12 23:32:55 +0100

    feat: Lombok in core module

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -3,3 +3,9 @@ plugins {
 }
 
 group = "me.vihara.atemwrapper.api"
+
+dependencies {
+    compileOnly("org.jetbrains:annotations:26.0.1")
+    compileOnly("org.projectlombok:lombok:1.18.36")
+    annotationProcessor("org.projectlombok:lombok:1.18.36")
+}
