44dba7f72878dd05b9e0ca4adfa78e0b7a879ff9
Alen Gorenjec <gorenjecalen@gmail.com>
2025-01-17 09:04:11 +0100

    feat: WIP ping scope

--- protocol/src/main/kotlin/me/vihara/atemwrapper/protocol/ProtocolClient.kt
+++ protocol/src/main/kotlin/me/vihara/atemwrapper/protocol/ProtocolClient.kt
@@ -3,6 +3,7 @@ package me.vihara.atemwrapper.protocol
 import io.ktor.network.selector.*
 import io.ktor.network.sockets.*
 import io.ktor.utils.io.*
+import io.ktor.utils.io.CancellationException
 import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.Channel
 
@@ -10,8 +11,11 @@ abstract class ProtocolClient(private val hostName: String, private val port: In
     private var socket: Socket? = null
     private var readJob: Job? = null
     private var writeJob: Job? = null
+    private var pingJob: Job? = null
     private val writeChannel = Channel<String>(Channel.UNLIMITED)
     private var ackDeferred: CompletableDeferred<Boolean>? = null
+
+    private val pingScope = CoroutineScope(Dispatchers.IO + SupervisorJob())
 
     suspend fun connect(): Socket {
         val selectorManager = SelectorManager(Dispatchers.IO)
@@ -22,15 +26,16 @@ abstract class ProtocolClient(private val hostName: String, private val port: In
         socket?.let {
             startReading(it)
             startWriting(it)
-            startPing()
         }
 
         return socket!!
     }
 
     fun disconnect() {
+        pingScope.cancel()
         readJob?.cancel()
         writeJob?.cancel()
+        pingJob?.cancel()
         writeChannel.close()
         socket?.close()
         socket = null
@@ -44,6 +49,9 @@ abstract class ProtocolClient(private val hostName: String, private val port: In
                     val data = input.readAvailable()
                     if (data.isNotEmpty()) {
                         processIncomingData(data)
+                        if (pingJob == null || pingJob?.isActive == false) {
+                            startPing()
+                        }
                     }
                 }
             } catch (e: Throwable) {
@@ -59,7 +67,6 @@ abstract class ProtocolClient(private val hostName: String, private val port: In
         writeJob = CoroutineScope(Dispatchers.IO).launch {
             try {
                 for (message in writeChannel) {
-                    println("Writing $message")
                     output.writeStringUtf8(message)
                 }
             } catch (e: Throwable) {
@@ -70,7 +77,6 @@ abstract class ProtocolClient(private val hostName: String, private val port: In
 
     private fun processIncomingData(data: ByteArray) {
         val message = data.decodeToString()
-        println("$message je msg")
         if (message.contains("ACK")) {
             ackDeferred?.complete(true)
         } else if (message.contains("NAK")) {
@@ -80,10 +86,18 @@ abstract class ProtocolClient(private val hostName: String, private val port: In
         }
     }
 
-    private suspend fun startPing() {
-        while (true) {
-            delay(1000)
-            sendCommand("PING:\n\n")
+    private fun startPing() {
+        pingJob = pingScope.launch {
+            try {
+                while (isActive) {
+                    delay(1000)
+                    sendCommand("PING:\n\n")
+                }
+            } catch (e: CancellationException) {
+                println("Ping task was cancelled")
+            } catch (e: Throwable) {
+                handleError(e)
+            }
         }
     }
 
@@ -91,7 +105,6 @@ abstract class ProtocolClient(private val hostName: String, private val port: In
         val termination = if (block.endsWith("\n\n")) "" else "\n"
         ackDeferred = CompletableDeferred()
         runBlocking {
-            println("Sending $block")
             writeChannel.send(block + termination)
             ackDeferred?.await()
         }
